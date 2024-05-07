352c228bb4e3c47ffc17a72ae436b59c0a389dda
davidwillson9 <2021.aniket.mahajan@ves.ac.in>
2024-05-07 12:45:31 +0530

    first commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-mongoose.connect('mongodb://localhost:27017/cafe', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://aniketmahajan2109:dnZ60stzihCGAPas@masynctech-cluster.ytzt319.mongodb.net/students-database', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connected to MongoDB');
     })
